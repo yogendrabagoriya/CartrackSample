07b99295acd70166ed057b53677bb7666c2f4b9f
Yogendra Bagoriya <yogendrabagoriya@gmail.com>
2022-12-02 01:38:30 +0530

    1. removing unwanted files.

--- Pods/Target Support Files/Pods-Carteck Sample-Carteck SampleUITests/Pods-Carteck Sample-Carteck SampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Carteck_Sample_Carteck_SampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Carteck_Sample_Carteck_SampleUITests
-@end
